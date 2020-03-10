b3881cf50b15245206407dfe85166dd382bff8eb
okhakumhe gideon <okhakumhegideon@gmail.com>
2020-03-10 19:09:18 +0100

    Implementation of Rabbit MQ

--- MicroRabbit/MicroRabbit.Domain.Core/Commands/Command.cs
+++ MicroRabbit/MicroRabbit.Domain.Core/Commands/Command.cs
@@ -10,7 +10,7 @@ namespace MicroRabbit.Domain.Core.Commands
     {
         public DateTime Timestamp { get; protected set; }
 
-        public Command()
+        protected Command()
         {
             Timestamp = DateTime.Now;
         }
