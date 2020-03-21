33e39a03aa78e806f4ef17c27a3bb8382d49391a
okhakumhe gideon <okhakumhegideon@gmail.com>
2020-03-21 21:56:59 +0100

    3/21/2020 added classes to IOC container

---
+++ MicroRabbit/MicroRabbit.Banking.Domain/Interface/IAccountRepository.cs
@@ -0,0 +1,12 @@
+﻿using MicroRabbit.Banking.Domain.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MicroRabbit.Banking.Domain.Interface
+{
+    public interface IAccountRepository
+    {
+        IEnumerable<Account> GetAccounts();
+    }
+}
